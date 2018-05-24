d4133292c4d5e253a5d05b762585c9dc47f56095
Marco Fringuelli <marco.fringuelli23@gmail.com>
2018-05-24 18:46:40 +0200

    Update LU.py

--- Python code/LU.py
+++ Python code/LU.py
@@ -1,40 +1,43 @@
-# Reference to http://quantstart.com/articles/LU-Decomposition-in-Python-and-NumPy
+# Reference to http://quantstart.com/articles/LU-Decomposition-in-Python-and-NumPy.
+# I have taken the code for LU fact. from here then I modified it to work with NumPy arrays
 from __future__ import print_function, division
-import cProfile
 from pprint import pprint
 import numba
 
-def mult_matrix(A, B):
-    # With zip() the matrix B is transformed into a tuple of columns, with list() into an array
-    unpB = list(zip(*B))
-    return [[sum(_a*_b for _a,_b in zip(rowa,colb)) for colb in unpB] for rowa in A]
+def dot_numpy(A,B):  # Pythonic implementation of matrices multiplication
+    return A@B
+
+def dot_py(A,B):  # Unpythonic implementation of matrices multiplication
+    L = len(A)
+    C = np.identity(L)
+    for i in range(L):
+        for j in range(L):
+            for k in range(L):
+                C[i,j] += A[i,k]*B[k,j]
+    return C
 
 def pivot_matrix(m):
-    """Creates the pivoting matrix for m (P)."""
-    # Compute the pivoting matrix that represent row exchanges in Gaussian elimination
     n = len(m)
-    # Creates an identity matrix with float values
-    ID = [[float(i == j) for i in range(n)] for j in range(n)]
-    # swaps by comparing max values of each row
+
+    ID = np.identity(n)
+   
     for j in range(n):
-        row = max(range(j, n), key=lambda i: abs(m[i][j]))
+        row = max(range(j, n), key=lambda i: abs(m[i][j]))  # Computes the row exchanges in the Gaussian elimination
         if j != row:
-            ID[j], ID[row] = ID[row], ID[j] # swapping 
+            ID[[j,row],:] = ID[[row,j],:] # swapping
     return ID
 
 def lu(A):
-    """Decomposes a nxn matrix A by PA=LU and returns L, U and P.
-    A must be square."""
     n = len(A)
-    # Initialization of empty L & U matrices
-    L = [[0.0] * n for i in range(n)]
-    U = [[0.0] * n for i in range(n)]
+    
+    L = np.identity(n)  # Initialization of empty L & U matrices
+    U = np.identity(n)
 
-    P = pivot_matrix(A)
-    A2 = mult_matrix(P, A)
+    P = pivot_matrix(A) # P is the Pivoting Matrix (PA = LU)
+    A2 = dot_py(P,A)
+    # A2 = dot_numpy(P,A)
 
     for j in range(n):
-        L[j][j] = 1.0
         for i in range(j+1):
             s1 = sum(U[k][j] * L[i][k] for k in range(i))
             U[i][j] = A2[i][j] - s1
@@ -43,5 +46,8 @@ def lu(A):
             L[i][j] = (A2[i][j] - s2) / U[j][j]
     return (L, U, P)
 
+A = np.array([[1.,3.,5.,9.,2.],
+[4.,0.,9.,-7.,-8.],
+[-4.,-3.,9.,-1.,5.]])
 # A = [[1,3,5],[4,0,9],[-4,-3,9]] test
-# print (lu(A))
+# pprint (lu(A))
